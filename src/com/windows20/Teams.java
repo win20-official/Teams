fcfcdbce6ca62067f73bbe77982698bdb3a55905
Windows 20 <80408876+win20-official@users.noreply.github.com>
2021-10-30 18:03:12 +0200

    Add files via upload

---
+++ src/com/windows20/Teams.java
@@ -0,0 +1,20 @@
+package com.windows20;
+
+import java.util.*;
+
+public class Teams {
+    public static void main(String[] args) {
+        // Teams by Windows 20
+        // Updates: 0
+        // Bugs: 0
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Teams 0.0.1 alpha");
+        System.out.println("Type your name: ");
+        System.out.println("Type your group name: ");
+        String name = sc.nextLine();
+        String group = sc.nextLine();
+        System.out.println("Hello " + name);
+        System.out.println("and welcome to the group named " + group);
+    }
+}
